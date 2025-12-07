4ce8f7603a063c47e975114c69520972300be4ac
Mangotimmy <73014565+Mangotimmy@users.noreply.github.com>
2025-12-07 21:54:15 +0800

    Rename user script to 'ewant_link_grab'

--- main.user.js
+++ main.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Manual Link Extractor (Fixed)
+// @name        ewant_link_grab
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  grab youtube link to open
